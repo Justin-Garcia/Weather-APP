d8f8b70f51abd1e98df492a99e59f4069f5a463c
Justin Garcia <justingarcia1901@gmail.com>
2024-03-04 17:48:55 -0500

    tweaks

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,26 @@
-// TODO: Grab API Key for Secondary Conversion API
 const apiKey = "31ebe040d9ce338865ece410970e4edd"
+const inputEl = document.getElementById(inputSearch)
+const searchBtn = document.getElementById(searchBtn)
+const temp = document.getElementById(degrees)
+const feelsLike = document.getElementById(feelsLike)
+const precipitation = document.getElementById(precipitation)
 
-
-// TODO: Register the search button click as submit, prevent default, have the input wiped out as the page is updated
 
 // TODO: Create fetch to translate city/location input to longitude - latitude input for openWeatherMap API
 
+function search(locationConverter) {
+    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&limit=${limit}&appid=${apiKey}`
+    fetch(url)
+        .then(res => res.json())
+        .then((data) => {
+            convertLocation(data.search)
+        })
+}
+
+
+
 function search(location) {
-    const url = `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=${apiKey}`
+    const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`
     fetch(url)
         .then(res => res.json())
         .then((data) => {
@@ -15,6 +28,17 @@ function search(location) {
         })
 }
 
+function displayWeather(data) {
+    console.log(data)
+    
+}
+
+
 // TODO: Create fetch request for Temp, Precip, Humidity, Weather
 
 
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    const search = inputEl.value
+    displayWeather(search)
+})
